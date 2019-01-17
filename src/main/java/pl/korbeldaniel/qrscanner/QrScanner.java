9d2d026fd03558823b284de532450b472b448084
Daniel Korbel <daniel.korbel.ext@deposit-solutions.com>
2019-01-17 21:03:55 +0100

    Removed toggle command

--- src/main/java/pl/korbeldaniel/qrscanner/QrScanner.java
+++ src/main/java/pl/korbeldaniel/qrscanner/QrScanner.java
@@ -6,20 +6,10 @@ import jsinterop.annotations.JsPackage;
 
 @JsType(namespace=JsPackage.GLOBAL, isNative=true)
 public class QrScanner {
-	private boolean isStarted;
 	
     public QrScanner(Element element, Decode onDecode, String widthAndHight) {};
     
     public native void start();
 	public native void stop();
-	
-	public void toggle() {
-		if(isStarted) {
-			this.isStarted = false;
-			this.stop();
-		} else {
-			this.isStarted = true;
-			this.start();
-		}
-	}
+
 }
